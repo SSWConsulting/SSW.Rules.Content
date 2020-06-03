b6bc522b51a5ac57b334fc439f6c7d8e061bba40
TiSeb <boissiere.seb@gmail.com>
2020-06-03 17:05:04 +1000

    Apply Css to rules

--- src/templates/rule.js
+++ src/templates/rule.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { graphql } from 'gatsby';
 import Layout from '../components/layout/layout';
 import PropTypes from 'prop-types';
+import {
+  faThumbsUp,
+  faThumbsDown
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 export default function Rule({
   data,
   pageContext: {
@@ -15,9 +20,53 @@ export default function Rule({
       crumbLabel={rule.frontmatter.title}
       displayActions={false}
     >
-      <div>
-        <h1>{rule.frontmatter.title}</h1>
-        <div dangerouslySetInnerHTML={{ __html: rule.html }} />
+      <div className="rule-single rounded">
+        <section className="rule-content p-12 mb-12">
+          <h1>{rule.frontmatter.title}</h1>
+          <small>
+            Created on {rule.frontmatter.created} | Last updated by{' '}
+            <a href="/profile">{rule.frontmatter.authors[0].title}</a>{' '}
+            {rule.frontmatter.created}
+          </small>
+          <hr />
+          <div dangerouslySetInnerHTML={{ __html: rule.html }} />
+
+          <section id="more" className="pt-12 mt-12 flex text-center">
+            <div className="acknowledgements w-1/3">
+              <h5>Acknowledgements</h5>
+              <div className="avatar">
+                <a href="/profile">
+                  <img
+                    className="rounded-full inline"
+                    src="https://adamcogan.com/wp-content/uploads/2018/07/adam-bw.jpg"
+                    alt="Adam Cogan"
+                  />
+                </a>
+              </div>
+            </div>
+            <div className="tags rounded w-1/3">
+              <h5>Tags</h5>
+              <span>Scrum</span> <span>Some other tag</span>{' '}
+              <span>One more</span> <span>And another</span>
+            </div>
+            <div className="likes w-1/3">
+              <h5>Feedback</h5>
+              <p>
+                <span>
+                  8 <FontAwesomeIcon icon={faThumbsUp} className="good" />
+                </span>{' '}
+                <span>
+                <FontAwesomeIcon icon={faThumbsDown} className="bad" /> 2
+                </span>
+              </p>
+              <p>
+                <small>
+                  <a href="/suggestion">Make a suggestion</a>
+                </small>
+              </p>
+            </div>
+          </section>
+        </section>
       </div>
     </Layout>
   );
@@ -34,6 +83,10 @@ export const query = graphql`
       html
       frontmatter {
         title
+        authors {
+          title
+        }
+        created(formatString: "DD MMM YYYY")
       }
     }
   }
