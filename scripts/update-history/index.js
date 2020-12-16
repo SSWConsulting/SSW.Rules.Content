559b5671f7d03ae90fb0ecec9edc86cf771c45a5
brendan richards <brendan_richards_msdn@hotmail.com>
2020-12-16 15:04:12 +1000

    debugging

--- scripts/update-history/index.js
+++ scripts/update-history/index.js
@@ -7,19 +7,22 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
+
+    // hgisotry files are written one level aboce the checked out work folder 
+
     let historyFile = fs.readFileSync('../../history.json');
     let historyData = JSON.parse(historyFile);
     console.log(`got ${historyData.length} history records`);
 
-    let addedFile = fs.readFileSync('../../files_added.json');
+    let addedFile = fs.readFileSync('../../../files_added.json');
     let addedData = JSON.parse(addedFile);
     console.log(`got ${addedData.length} added files`);
 
-    let modifiedFile = fs.readFileSync('../../files_modified.json');
+    let modifiedFile = fs.readFileSync('../../../files_modified.json');
     let modifiedData = JSON.parse(modifiedFile);
     console.log(`got ${modifiedData.length} modified files`);
 
-    let file = fs.readFileSync('../../files.json');
+    let file = fs.readFileSync('../../../files.json');
     let data = JSON.parse(file);
 
  
