b3e687e19b2efe5818b19bb9a1dafcf618ca6ad8
brendan richards <brendan_richards_msdn@hotmail.com>
2020-12-16 14:43:29 +1000

    debug relative paths

--- scripts/update-history/index.js
+++ scripts/update-history/index.js
@@ -9,19 +9,20 @@ const github = require('@actions/github');
 try {
     let historyFile = fs.readFileSync('../../history.json');
     let historyData = JSON.parse(historyFile);
+    console.log(`got ${historyData.length} history records`);
 
     let addedFile = fs.readFileSync('../../files_added.json');
     let addedData = JSON.parse(addedFile);
+    console.log(`got ${addedData.length} added files`);
 
     let modifiedFile = fs.readFileSync('../../files_modified.json');
     let modifiedData = JSON.parse(modifiedFile);
+    console.log(`got ${modifiedData.length} modified files`);
 
     let file = fs.readFileSync('../../files.json');
     let data = JSON.parse(file);
 
-    console.log(`got ${historyData.length} history records`);
-    console.log(`got ${addedData.length} added files`);
-    console.log(`got ${modifiedData.length} modified files`);
+ 
     console.log(`got ${data.length} from files.json`);
 
 } catch (error) {
