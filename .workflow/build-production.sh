128800d481896d586284f8e36186c32b41db9190
Christian Morford-Waite [SSW] <ChristianMWaite@ssw.com.au>
2021-02-02 13:44:59 +1100

    Add build triggers (#19)
    * Create TriggerDevOpsBuild.yml
    
    * Create build-production.sh

---
+++ .workflow/build-production.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+github_org_name=sswconsulting
+github_repo_name=SSW.Rules
+azdo_org_name=https://ssw.visualstudio.com
+azdo_project_name=ssw.rules
+azdo_pipeline_name=Production
+
+# exit when any command fails
+set -e
+
+# ensure devops extension is available
+az extension add -n azure-devops
+
+# get latest release branch from other repo
+echo Querying GitHub...
+branch_list=$(curl -s -X GET https://api.github.com/repos/${github_org_name}/${github_repo_name}/branches) 
+branch_name=$(jq -r  '[.[].name | select(startswith("release/"))] | sort_by(.[8:]|tonumber) | reverse | .[0]' <<< "${branch_list}") 
+echo Latest release branch: ${branch_name}
+
+echo triggering AzDO build
+az pipelines build queue --org $azdo_org_name --project $azdo_project_name --definition-name $azdo_pipeline_name --branch $branch_name
+
+echo done
