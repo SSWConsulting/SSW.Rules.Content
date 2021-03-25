9843fbb6290257db54745269824c71cdcd0db1b6
Brady Stroud <bradystroud@me.com>
2021-03-26 08:57:20 +1000

    Update script

--- .workflow/cancelPreviousBuilds.ps1
+++ .workflow/cancelPreviousBuilds.ps1
@@ -1,23 +1,31 @@
-$pair = "$($env:CANCEL_BUILD_USER):$($env:CANCEL_BUILD_TOKEN)"
+if ([string]::IsNullOrEmpty(($env:CANCEL_BUILD_USER)) -or [string]::IsNullOrEmpty(($env:CANCEL_BUILD_TOKEN))) {
+    throw "Missing user environment variables"
+    exit
+}
+if ([string]::IsNullOrEmpty(($env:CANCEL_BUILD_URL))) {
+    throw "Missing project URL environment variables"
+    exit
+}
 
-$encodedCreds = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($pair))
-
+$creds = "$($env:CANCEL_BUILD_USER):$($env:CANCEL_BUILD_TOKEN)"
+$encodedCreds = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($creds))
 $basicAuthValue = "Basic $encodedCreds"
-
 $Headers = @{
     Authorization = $basicAuthValue
 }
 
 $buildsUrl = $env:CANCEL_BUILD_URL
+
 $builds = Invoke-RestMethod -Uri $buildsUrl -Method Get -Header $Headers
 $buildsToStop = $builds.value.Where( { (($_.status -eq 'inProgress') -or ($_.status -eq 'notStarted')) -and ([string]::IsNullOrEmpty(($_.triggerInfo))) })
+
 $count = ($buildsToStop).count
 Write-Host "Builds to cancel:"
 Write-Host $count
+
 ForEach ($build in $buildsToStop) {
     $build.status = "Cancelling"
     $body = $build | ConvertTo-Json -Depth 10
     $urlToCancel = "$($buildsUrl)/$($build.id)?api-version=5.0"
     Invoke-RestMethod -Uri $urlToCancel -Method Patch -ContentType application/json -Body $body -Header $Headers
-    Write-Host $count + "builds remaining"
 }
