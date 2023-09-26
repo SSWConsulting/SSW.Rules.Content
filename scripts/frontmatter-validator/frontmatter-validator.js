f804f57950b0081ffab9f693486fc0ed30a3686a
ChloeLin <chloelin@ssw.com.au>
2023-09-26 14:36:30 +0800

    load more schemas for validation

--- scripts/frontmatter-validator/frontmatter-validator.js
+++ scripts/frontmatter-validator/frontmatter-validator.js
@@ -4,25 +4,37 @@ const yaml = require('js-yaml');
 const addFormats = require('ajv-formats');
 const ajvErrors = require('ajv-errors');
 
-const schema = JSON.parse(fs.readFileSync('rule-schema.json', 'utf8'));
+const schemas = {
+  rule: loadSchema('rule-schema.json'),
+  category: loadSchema('category-schema.json'),
+  top_category: loadSchema('top-category-schema.json'),
+}
 
-const validator = new ajv({ allErrors: true });
-addFormats(validator);
-ajvErrors(validator);
-const validate = validator.compile(schema);
+const validator = initializeValidator()
 
-function main() {
-  const eventType = process.env.GITHUB_EVENT_NAME;
+function initializeValidator() {
+  const validator = new ajv({ allErrors: true });
+  addFormats(validator);
+  ajvErrors(validator);
 
-  if (eventType === 'pull_request') {
-    if (process.argv[2] && process.argv[2].length > 0) {
-      const folders = process.argv[2]
-        .split(',')
-        .filter((file) => file.endsWith('rule.md'))
-        .map((file) => `../../${file}`);
+  for (const key in schemas) {
+    validator.addSchema(schemas[key], key);
+  }
 
-      folders.forEach((file) => validateFrontmatter(file));
-    }
+  return validator
+}
+
+function loadSchema(schemaPath) {
+  return JSON.parse(fs.readFileSync(schemaPath, 'utf8'));
+}
+
+function matchSchema(filePath) {
+  if (filePath.endsWith('rule.md')) {
+    return validator.getSchema('rule')
+  } else if (filePath.indexOf('/categories') !== -1 && !filePath.endsWith('index.md')) {
+    return validator.getSchema('category')
+  } else if (filePath.indexOf('/categories') !== -1 && filePath.endsWith('index.md')) {
+    return validator.getSchema('top_category')
   }
 }
 
@@ -30,6 +42,7 @@ function validateFrontmatter(filePath) {
   const fileContents = fs.readFileSync(filePath, 'utf8');
   const frontmatter = parseFrontmatter(fileContents);
 
+  const validate = matchSchema(filePath)
   const isValid = validate(frontmatter);
 
   if (!isValid) {
@@ -58,6 +71,21 @@ function parseFrontmatter(fileContents) {
   } catch (error) {
     console.error(`Error parsing frontmatter: missing '---'`);
     process.exit(1);
+  }
+}
+
+function main() {
+  const eventType = process.env.GITHUB_EVENT_NAME;
+
+  if (eventType === 'pull_request') {
+    if (process.argv[2] && process.argv[2].length > 0) {
+      const folders = process.argv[2]
+        .split(',')
+        .filter((file) => file.endsWith('.md'))
+        .map((file) => `../../${file}`);
+
+      folders.forEach((file) => validateFrontmatter(file));
+    }
   }
 }
 
