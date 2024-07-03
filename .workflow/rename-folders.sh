98e683e424d79fed8921c4a069c7dfefdfa5c832
Brady Stroud [SSW] <bradystroud@ssw.com.au>
2024-07-03 14:15:25 +0930

    mrga

--- .workflow/rename-folders.sh
+++ .workflow/rename-folders.sh
@@ -9,7 +9,7 @@ for rule_file in $(git diff --name-only $(git merge-base origin/main HEAD) | gre
   echo "Folder name: $folder_name"
   
   uri=$(grep -m 1 '^uri:' "${GITHUB_WORKSPACE}/$rule_file" | awk '{print $2}')
-  uri = $($uri | tr -d '\r')
+  uri=$($uri | tr -d '\r')
 
   echo "URI: $uri"
   
